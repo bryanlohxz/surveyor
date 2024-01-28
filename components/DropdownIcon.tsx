ee53834ea9c886110fc2fd2f152df095fd0954d2
Bryan Loh <bryanlohxz@gmail.com>
2024-01-28 17:04:43 +0800

    Add create survey form

---
+++ components/DropdownIcon.tsx
@@ -0,0 +1,25 @@
+interface Props {
+  className?: string;
+}
+
+const DropdownIcon: React.FC<Props> = ({ className }) => {
+  return (
+    <svg
+      className={`w-2.5 h-2.5 ms-3 ${className}`}
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 10 6"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m1 1 4 4 4-4"
+      />
+    </svg>
+  );
+};
+
+export default DropdownIcon;
